9f7923e3eb293769a7f390fa5544b5ea0ff78155
FoskyM <i@fosky.top>
2024-09-25 13:57:42 +0800

    init repo

---
+++ app/src/main/java/net/fosky/sticker/sticker_sword/hook/HookEntry.kt
@@ -0,0 +1,18 @@
+package net.fosky.sticker.sticker_sword.hook
+
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
+@InjectYukiHookWithXposed
+class HookEntry : IYukiHookXposedInit {
+
+    override fun onInit() = configs {
+        // Your code here.
+    }
+
+    override fun onHook() = encase {
+        // Your code here.
+    }
+}
