24e67b75fe33a38f88ed9619597f99758d58b03c
FoskyM <i@fosky.top>
2024-10-01 15:21:08 +0800

    chore(WechatHooker): comment temporarily

--- app/src/main/java/net/fosky/sticker/sticker_sword/hook/entity/WechatHooker.kt
+++ app/src/main/java/net/fosky/sticker/sticker_sword/hook/entity/WechatHooker.kt
@@ -22,6 +22,10 @@
 package net.fosky.sticker.sticker_sword.hook.entity
 
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.factory.allConstructors
+import com.highcapable.yukihookapi.hook.factory.allMethods
+import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.log.YLog
 import net.fosky.sticker.sticker_sword.const.PackageName
 
 object WechatHooker : YukiBaseHooker()  {
@@ -33,9 +37,71 @@ object WechatHooker : YukiBaseHooker()  {
     val SmileyInfo by lazyClassOrNull("${PackageName.WECHAT}.storage.emotion.SmileyInfo")
     val SmileyPanelConfigInfo by lazyClassOrNull("${PackageName.WECHAT}.storage.emotion.SmileyPanelConfigInfo")
     val MMAnimateView by lazyClassOrNull("${PackageName.WECHAT}.plugin.gif.MMAnimateView")
-
-
+    val EmojiPanelRecyclerView by lazyClassOrNull("${PackageName.WECHAT}.emoji.panel.EmojiPanelRecyclerView")
+    val EmojiPanelGroupView by lazyClassOrNull("${PackageName.WECHAT}.emoji.panel.EmojiPanelGroupView")
+    // 没摸明白怎么调用的
+    // EmojiInfo 在微信启动时构造，看了一下是数据库读取的，混淆了类名和方法，不好调用
+    // 后面没找到是哪个View调用了读取，先扔这里了
     override fun onHook() {
-        TODO("Not yet implemented")
+        YLog.info("Hooking Wechat")
+//        EmojiPanelGroupView?.allConstructors { _, constructor ->
+//            constructor.hook().before {
+//                var argString = ""
+//                for (arg in args) {
+//                    argString += arg.toString() + "\n"
+//                }
+//                val traceElements = Thread.currentThread().stackTrace
+//                var stackTraceString = ""
+//                for (traceElement in traceElements) {
+//                    stackTraceString += traceElement.toString() + "\n"
+//                }
+//                YLog.debug("EmojiPanelGroupView.constructor\n$stackTraceString")
+//            }
+//        }
+//        EmojiPanelRecyclerView?.allConstructors { _, constructor ->
+//            constructor.hook().before {
+//                var argString = ""
+//                for (arg in args) {
+//                    argString += arg.toString() + "\n"
+//                }
+//                val traceElements = Thread.currentThread().stackTrace
+//                var stackTraceString = ""
+//                for (traceElement in traceElements) {
+//                    stackTraceString += traceElement.toString() + "\n"
+//                }
+//                YLog.debug("EmojiPanelRecyclerView.constructor\n$stackTraceString")
+//            }
+//        }
+
+//        EmojiInfo?.allConstructors { _, constructor ->
+//            constructor.hook().before {
+//                var argString = ""
+//                for (arg in args) {
+//                    argString += arg.toString() + "\n"
+//                }
+////                YLog.debug("EmojiInfo.constructor, args: $argString")
+//                val traceElements = Thread.currentThread().stackTrace
+//                var stackTraceString = ""
+//                for (traceElement in traceElements) {
+//                    stackTraceString += traceElement.toString() + "\n"
+//                }
+//                YLog.debug("EmojiInfo.constructor\n$stackTraceString")
+//            }
+//        }
+//        EmojiInfo?.allMethods { _, method ->
+//            method.hook().before {
+//                if (method.name == "F0") return@before
+//                var argString = ""
+//                for (arg in args) {
+//                    argString += arg.toString() + "\n"
+//                }
+//                val traceElements = Thread.currentThread().stackTrace
+//                var stackTraceString = ""
+//                for (traceElement in traceElements) {
+//                    stackTraceString += traceElement.toString() + "\n"
+//                }
+//                YLog.debug("EmojiInfo.method: ${method.name}\n${argString}\n$stackTraceString")
+//            }
+//        }
     }
 }
